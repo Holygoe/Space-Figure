ccbf995f47b35817d0be2d135a89407c049708de
Denis Kondratev <kondratev.da@gmail.com>
2020-10-23 16:53:36 +0300

    Commit

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
+using UnityEngine.InputSystem.EnhancedTouch;
 
 namespace RudeBlooper.SpaceFigure
 {
     public class GameManager : MonoBehaviour
     {
-
+        private void Start()
+        {
+            TouchSimulation.Enable();
+        }
     }
 }
